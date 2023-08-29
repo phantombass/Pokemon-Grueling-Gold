1de32db8c6f1ff4cd5341c2293eb97c040a8e297
phantombass <tmkeaton@ngu.edu>
2023-08-29 00:38:42 -0400

    0.2.8

--- new_files.sh
+++ new_files.sh
@@ -1,4 +1,12 @@
 #!/Data/Updater/bin/sh
 version=`cat version`
-curl -o Data/Changes/${version}/changes https://raw.githubusercontent.com/phantombass/Pokemon-Grueling-Gold/Release/Data/Changes/${version}/changes
-curl -o Data/Changes/${version}/deleted https://raw.githubusercontent.com/phantombass/Pokemon-Grueling-Gold/Release/Data/Changes/${version}/deleted
+files=`cat Data/Changes/${version}`
+curl -o Data/Changes/${version}/changes https://raw.githubusercontent.com/phantombass/Pokemon-Grueling-Gold/Release/${files}/changes
+curl -o Data/Changes/${version}/deleted https://raw.githubusercontent.com/phantombass/Pokemon-Grueling-Gold/Release/${files}/deleted
+curl -o "version_temp" https://raw.githubusercontent.com/phantombass/Pokemon-Grueling-Gold/master/version
+link=`cat version_temp`
+echo 'mkfile() { mkdir -p "$(dirname "$1")" && touch "$1" ;  }' >> ~/.bashrc
+source ~/.bashrc
+mkfile Data/Changes/${link}/changes
+mkfile Data/Changes/${link}/deleted
+rm version_temp
